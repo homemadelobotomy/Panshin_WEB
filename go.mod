7e61efac85813727bea3baf84f14e252fb4412e6
homemadelobotomy <panshinmv@gmail.com>
2025-09-18 16:22:07 +0300

    begin

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lab
+
+go 1.23.0
+
+toolchain go1.24.7
---
+++ internal/app/repository/repository.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func New(dsn string) (*Repository, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+}
